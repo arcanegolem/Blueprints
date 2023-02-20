a911f8706340ca024d19ab60722c771bf7aacee2
arcanegolem <miv738@gmail.com>
2023-02-21 01:40:56 +0300

    Removed unused imports

--- app/src/main/java/com/mockups/blueprints/navigation/Navigator.kt
+++ app/src/main/java/com/mockups/blueprints/navigation/Navigator.kt
@@ -1,19 +1,11 @@
 package com.mockups.blueprints.navigation
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import com.mockups.blueprints.animation.LoadingScreen
 import com.mockups.blueprints.animation.MainScreenComposable
-import com.mockups.blueprints.blocks.CollapsableBox
 
 @Composable
 fun Navigator(navController: NavHostController){
